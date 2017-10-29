ce7732dc4e2f01e33110041f63afe4671c4224cb
Victor Darras <victor.darras@gmail.com>
2017-10-29 16:48:15 +0100

    Add a dev Rake task

--- lib/tasks/redspot.rb
+++ lib/tasks/redspot.rb
@@ -1,7 +1,14 @@
 # Rakefile
+
 desc "Build the Redspot"
-task :redspot_build do
+task :build do
   Dir.chdir(File.expand_path("../../../redspot_vuejs", __FILE__))
   sh 'npm install && npm run build'
   puts "Now built, enjoy"
+end
+
+desc "Watch & Expose the Redspot at localhost:8080"
+task :dev do
+  Dir.chdir(File.expand_path("../../../redspot_vuejs", __FILE__))
+  sh 'npm run dev'
 end
